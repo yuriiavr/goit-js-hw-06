0fbb5fbf60d2a9ba4776840e8e1d4002d44bf360
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2022-09-21 19:37:26 +0300

    Delete src directory

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const numberOfCategories = document.querySelectorAll("li.item").length;
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${numberOfCategories}`);
-categories.forEach((category) => {
-    console.log(`Category: ${category.querySelector("h2").textContent}`);
-    console.log(`Elements: ${category.querySelectorAll("li").length}`);
-})
--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector('#font-size-control');
-const textInputSize = document.querySelector('#text');
-
-textInput.addEventListener("input", (event) => {
-    textInputSize.style.fontSize = event.currentTarget.value + "px";
-});
